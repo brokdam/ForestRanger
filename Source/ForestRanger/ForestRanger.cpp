591b9523ebe06f684148fef0b6f0dd0cf8bb9d99
brogdam <brogdam208@kakao.com>
2026-06-11 12:52:59 +0900

    프로젝트 기본 세팅

---
+++ Source/ForestRanger/ForestRanger.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ForestRanger.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ForestRanger, "ForestRanger" );
+ 
---
+++ Source/ForestRanger/ForestRangerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ForestRangerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AForestRangerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AForestRangerGameMode();
+};
+
+
+
